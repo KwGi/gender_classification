7ed165f3c3d61918fdc6b38906bc19bdd43f9c29
Kwstas_Giantsios <38194761+KwGi@users.noreply.github.com>
2018-07-04 19:44:55 +0300

    Added 3  more classifiers
    made a list from classifiers to make it easier to fit  and predicts , also i used accuracy score  fuction from sklearn.metrics  to compare the  classifiers

--- demo.py
+++ demo.py
@@ -1,12 +1,11 @@
 from sklearn import tree
+from sklearn import ensemble
+from sklearn import naive_bayes
+from sklearn import discriminant_analysis
+from  sklearn.metrics import accuracy_score
 
-clf = tree.DecisionTreeClassifier()
-
-# CHALLENGE - create 3 more classifiers...
-# 1
-# 2
-# 3
-
+clf= [tree.DecisionTreeClassifier(),ensemble.AdaBoostClassifier(), naive_bayes.GaussianNB(),
+    discriminant_analysis.QuadraticDiscriminantAnalysis()] ;
 # [height, weight, shoe_size]
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
      [190, 90, 47], [175, 64, 39],
@@ -17,10 +16,52 @@ Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
 
 
 # CHALLENGE - ...and train them on our data
-clf = clf.fit(X, Y)
+#train on data
+for  i in range(len(clf)):
+     clf[i] = clf[i].fit(X, Y)
 
-prediction = clf.predict([[190, 70, 43]])
 
-# CHALLENGE compare their reusults and print the best one!
+#user enter his/her data
+height = input("Enter your height:")
+weight =  input("Enter your weight:")
+shoe_size = input("Enter your shoe_size:")
+y_true = input("Enter your actual gender:")
+inputdata = [int(height),int(weight),int(shoe_size)]
 
-print(prediction)
+#calculate Predictions
+prediction = []
+for  i in range(len(clf)):
+    prediction.append(clf[i].predict([inputdata]))
+
+#calculate Scores
+score =  []
+for i in range(len(prediction)):
+        score.append(accuracy_score([0] if y_true == "male" else [1] , [0] if prediction[i] == "male" else [1]))
+
+print("\n Predictions: ")
+#print predicts
+for  i in range(len(prediction)):
+    if i == 0 :
+        print("DecisionTreeClassifier predicts: ",end = "")
+    elif  i == 1:
+        print("AdaBoostClassifier predicts: ",end = "")
+    elif i == 2 :
+        print("GaussianNB predicts: ",end = "")
+    else :
+        print("QuadraticDiscriminantAnalysis predicts: ",end = "")
+    print(prediction[i])
+    print("")
+
+print("\n Scores: (1 -> right prediction, 0 -> wrong prediction)")
+#print Scores
+for  i in range(len(score)):
+    if i == 0 :
+        print("DecisionTreeClassifier scores: ",end = "")
+    elif  i == 1:
+        print("AdaBoostClassifier scores: ",end = "")
+    elif i == 2 :
+        print("GaussianNB scores: ",end = "")
+    else :
+        print("QuadraticDiscriminantAnalysis scores: ",end = "")
+    print(score[i])
+    print("")
